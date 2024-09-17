e2f5d6f248e4648456c8101c21d8fef10a6d2cff
Ådne-Sigurd Leirdal <asleirdal@uia.no>
2024-09-17 18:59:46 +0200

    Replaced BladeRF with USRP in GNURadio

--- GNURadio/apps/lora_b210.py
+++ GNURadio/apps/lora_b210.py
@@ -5,59 +5,46 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: Lora Bladerf
-# Author: Tapparel Joachim@EPFL,TCL
-# GNU Radio version: 3.10.1.1
-
-from packaging.version import Version as StrictVersion
-
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print("Warning: failed to XInitThreads()")
+# Title: Lora B210
+# Author: Ådne Sigurd Leirdal & Gustav Halvorsen
+# GNU Radio version: 3.10.10.0
 
 from PyQt5 import Qt
 from gnuradio import qtgui
-from gnuradio.filter import firdes
-import sip
 from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import filter
+from gnuradio.filter import firdes
 from gnuradio import gr
 from gnuradio.fft import window
 import sys
 import signal
+from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
+from gnuradio import uhd
+import time
 from gnuradio import zeromq
 from xmlrpc.server import SimpleXMLRPCServer
 import threading
 import gnuradio.lora_sdr as lora_sdr
 import numpy as np
-import osmosdr
-import time
+import sip
 
 
 
-from gnuradio import qtgui
-
-class lora_bladerf(gr.top_block, Qt.QWidget):
+class lora_b210(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Lora Bladerf", catch_exceptions=True)
+        gr.top_block.__init__(self, "Lora B210", catch_exceptions=True)
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("Lora Bladerf")
+        self.setWindowTitle("Lora B210")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
         self.top_scroll_layout = Qt.QVBoxLayout()
         self.setLayout(self.top_scroll_layout)
         self.top_scroll = Qt.QScrollArea()
@@ -70,15 +57,14 @@ class lora_bladerf(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "lora_bladerf")
+        self.settings = Qt.QSettings("GNU Radio", "lora_b210")
 
         try:
-            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-                self.restoreGeometry(self.settings.value("geometry").toByteArray())
-            else:
-                self.restoreGeometry(self.settings.value("geometry"))
-        except:
-            pass
+            geometry = self.settings.value("geometry")
+            if geometry:
+                self.restoreGeometry(geometry)
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
 
         ##################################################
         # Variables
@@ -94,23 +80,72 @@ class lora_bladerf(gr.top_block, Qt.QWidget):
         self.cr = cr = 1
         self.center_freq = center_freq = 868100000.0
         self.bw = bw = 125000
+        self.bandwidth = bandwidth = 2e6
 
         ##################################################
         # Blocks
         ##################################################
-        self.zeromq_push_sink_0_1 = zeromq.push_sink(gr.sizeof_char, 1, 'tcp://127.0.0.1:5559', 100, False, -1)
-        self.zeromq_push_sink_0_0_1 = zeromq.push_sink(gr.sizeof_char, 1, 'tcp://127.0.0.1:5560', 100, False, -1)
-        self.zeromq_push_sink_0_0_0_0 = zeromq.push_sink(gr.sizeof_char, 1, 'tcp://127.0.0.1:5558', 100, False, -1)
-        self.zeromq_push_sink_0_0_0 = zeromq.push_sink(gr.sizeof_char, 1, 'tcp://127.0.0.1:5561', 100, False, -1)
-        self.zeromq_push_sink_0_0 = zeromq.push_sink(gr.sizeof_char, 1, 'tcp://127.0.0.1:5557', 100, False, -1)
-        self.zeromq_push_sink_0 = zeromq.push_sink(gr.sizeof_char, 1, 'tcp://127.0.0.1:5556', 100, False, -1)
-        self.zeromq_pull_source_0_0 = zeromq.pull_source(gr.sizeof_char, 1, 'tcp://127.0.0.1:5554', 100, False, -1)
-        self.zeromq_pull_source_0 = zeromq.pull_source(gr.sizeof_char, 1, 'tcp://127.0.0.1:5555', 100, False, -1)
+
+        self.zeromq_push_sink_0_1 = zeromq.push_sink(gr.sizeof_char, 1, 'tcp://127.0.0.1:5559', 100, False, (-1), True)
+        self.zeromq_push_sink_0_0_1 = zeromq.push_sink(gr.sizeof_char, 1, 'tcp://127.0.0.1:5560', 100, False, (-1), True)
+        self.zeromq_push_sink_0_0_0_0 = zeromq.push_sink(gr.sizeof_char, 1, 'tcp://127.0.0.1:5558', 100, False, (-1), True)
+        self.zeromq_push_sink_0_0_0 = zeromq.push_sink(gr.sizeof_char, 1, 'tcp://127.0.0.1:5561', 100, False, (-1), True)
+        self.zeromq_push_sink_0_0 = zeromq.push_sink(gr.sizeof_char, 1, 'tcp://127.0.0.1:5557', 100, False, (-1), True)
+        self.zeromq_push_sink_0 = zeromq.push_sink(gr.sizeof_char, 1, 'tcp://127.0.0.1:5556', 100, False, (-1), True)
+        self.zeromq_pull_source_0_0 = zeromq.pull_source(gr.sizeof_char, 1, 'tcp://127.0.0.1:5554', 100, False, (-1), False)
+        self.zeromq_pull_source_0 = zeromq.pull_source(gr.sizeof_char, 1, 'tcp://127.0.0.1:5555', 100, False, (-1), False)
         self.xmlrpc_server_0 = SimpleXMLRPCServer(('localhost', 8089), allow_none=True)
         self.xmlrpc_server_0.register_instance(self)
         self.xmlrpc_server_0_thread = threading.Thread(target=self.xmlrpc_server_0.serve_forever)
         self.xmlrpc_server_0_thread.daemon = True
         self.xmlrpc_server_0_thread.start()
+        self.uhd_usrp_source_0 = uhd.usrp_source(
+            ",".join(("", '')),
+            uhd.stream_args(
+                cpu_format="fc32",
+                args='',
+                channels=list(range(0,1)),
+            ),
+        )
+        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_source_0.set_time_unknown_pps(uhd.time_spec(0))
+
+        self.uhd_usrp_source_0.set_center_freq(center_freq, 0)
+        self.uhd_usrp_source_0.set_antenna("TX/RX", 0)
+        self.uhd_usrp_source_0.set_bandwidth(bandwidth, 0)
+        self.uhd_usrp_source_0.set_gain(10, 0)
+        self.uhd_usrp_sink_0_0 = uhd.usrp_sink(
+            ",".join(("", '')),
+            uhd.stream_args(
+                cpu_format="fc32",
+                args='',
+                channels=list(range(0,1)),
+            ),
+            "",
+        )
+        self.uhd_usrp_sink_0_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_sink_0_0.set_time_unknown_pps(uhd.time_spec(0))
+
+        self.uhd_usrp_sink_0_0.set_center_freq(center_freq, 0)
+        self.uhd_usrp_sink_0_0.set_antenna("TX/RX", 0)
+        self.uhd_usrp_sink_0_0.set_bandwidth(125e3, 0)
+        self.uhd_usrp_sink_0_0.set_gain(10, 0)
+        self.uhd_usrp_sink_0 = uhd.usrp_sink(
+            ",".join(("", '')),
+            uhd.stream_args(
+                cpu_format="fc32",
+                args='',
+                channels=list(range(0,1)),
+            ),
+            "",
+        )
+        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_sink_0.set_time_unknown_pps(uhd.time_spec(0))
+
+        self.uhd_usrp_sink_0.set_center_freq(center_freq, 0)
+        self.uhd_usrp_sink_0.set_antenna("TX/RX", 0)
+        self.uhd_usrp_sink_0.set_bandwidth(125e3, 0)
+        self.uhd_usrp_sink_0.set_gain(10, 0)
         self.qtgui_waterfall_sink_x_0 = qtgui.waterfall_sink_c(
             1024, #size
             window.WIN_BLACKMAN_hARRIS, #wintype
@@ -156,7 +191,7 @@ class lora_bladerf(gr.top_block, Qt.QWidget):
             None # parent
         )
         self.qtgui_freq_sink_x_0_1.set_update_time(0.10)
-        self.qtgui_freq_sink_x_0_1.set_y_axis(-140, 10)
+        self.qtgui_freq_sink_x_0_1.set_y_axis((-140), 10)
         self.qtgui_freq_sink_x_0_1.set_y_label('Relative Gain', 'dB')
         self.qtgui_freq_sink_x_0_1.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
         self.qtgui_freq_sink_x_0_1.enable_autoscale(False)
@@ -188,45 +223,6 @@ class lora_bladerf(gr.top_block, Qt.QWidget):
 
         self._qtgui_freq_sink_x_0_1_win = sip.wrapinstance(self.qtgui_freq_sink_x_0_1.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_1_win)
-        self.osmosdr_source_1 = osmosdr.source(
-            args="numchan=" + str(1) + " " + 'bladerf=0'
-        )
-        self.osmosdr_source_1.set_time_unknown_pps(osmosdr.time_spec_t())
-        self.osmosdr_source_1.set_sample_rate(2000000)
-        self.osmosdr_source_1.set_center_freq(868000000, 0)
-        self.osmosdr_source_1.set_freq_corr(0, 0)
-        self.osmosdr_source_1.set_dc_offset_mode(0, 0)
-        self.osmosdr_source_1.set_iq_balance_mode(0, 0)
-        self.osmosdr_source_1.set_gain_mode(False, 0)
-        self.osmosdr_source_1.set_gain(10, 0)
-        self.osmosdr_source_1.set_if_gain(20, 0)
-        self.osmosdr_source_1.set_bb_gain(20, 0)
-        self.osmosdr_source_1.set_antenna('', 0)
-        self.osmosdr_source_1.set_bandwidth(2000000, 0)
-        self.osmosdr_sink_0_0_0 = osmosdr.sink(
-            args="numchan=" + str(1) + " " + 'bladerf=0'
-        )
-        self.osmosdr_sink_0_0_0.set_time_unknown_pps(osmosdr.time_spec_t())
-        self.osmosdr_sink_0_0_0.set_sample_rate(2000000)
-        self.osmosdr_sink_0_0_0.set_center_freq(sink_freq2, 0)
-        self.osmosdr_sink_0_0_0.set_freq_corr(0, 0)
-        self.osmosdr_sink_0_0_0.set_gain(10, 0)
-        self.osmosdr_sink_0_0_0.set_if_gain(20, 0)
-        self.osmosdr_sink_0_0_0.set_bb_gain(20, 0)
-        self.osmosdr_sink_0_0_0.set_antenna('', 0)
-        self.osmosdr_sink_0_0_0.set_bandwidth(bw, 0)
-        self.osmosdr_sink_0_0 = osmosdr.sink(
-            args="numchan=" + str(1) + " " + 'bladerf=0'
-        )
-        self.osmosdr_sink_0_0.set_time_unknown_pps(osmosdr.time_spec_t())
-        self.osmosdr_sink_0_0.set_sample_rate(2000000)
-        self.osmosdr_sink_0_0.set_center_freq(sink_freq2, 0)
-        self.osmosdr_sink_0_0.set_freq_corr(0, 0)
-        self.osmosdr_sink_0_0.set_gain(10, 0)
-        self.osmosdr_sink_0_0.set_if_gain(20, 0)
-        self.osmosdr_sink_0_0.set_bb_gain(20, 0)
-        self.osmosdr_sink_0_0.set_antenna('', 0)
-        self.osmosdr_sink_0_0.set_bandwidth(bw, 0)
         self.low_pass_filter_0_0_0 = filter.fir_filter_ccf(
             1,
             firdes.low_pass(
@@ -257,10 +253,10 @@ class lora_bladerf(gr.top_block, Qt.QWidget):
                 window.WIN_HAMMING,
                 6.76))
         self.low_pass_filter_0.set_min_output_buffer(65568)
-        self.lora_sdr_whitening_0_0 = lora_sdr.whitening(True,',')
-        self.lora_sdr_whitening_0 = lora_sdr.whitening(True,',')
-        self.lora_sdr_modulate_0_0 = lora_sdr.modulate(sf, 2000000, bw, [0x34], int(20*2**sf*samp_rate/bw),8)
-        self.lora_sdr_modulate_0 = lora_sdr.modulate(sink_sf2, 2000000, bw, [0x34], int(20*2**sink_sf2*samp_rate/bw),8)
+        self.lora_sdr_whitening_0_0 = lora_sdr.whitening(True,False,',','packet_len')
+        self.lora_sdr_whitening_0 = lora_sdr.whitening(True,False,',','packet_len')
+        self.lora_sdr_modulate_0_0 = lora_sdr.modulate(sf, 2000000, bw, [0x34], (int(20*2**sf*samp_rate/bw)),8)
+        self.lora_sdr_modulate_0 = lora_sdr.modulate(sink_sf2, 2000000, bw, [0x34], (int(20*2**sink_sf2*samp_rate/bw)),8)
         self.lora_sdr_interleaver_0_0 = lora_sdr.interleaver(cr, sf, 2, bw)
         self.lora_sdr_interleaver_0 = lora_sdr.interleaver(cr, sink_sf2, 2, bw)
         self.lora_sdr_header_decoder_0_1 = lora_sdr.header_decoder(impl_head, 3, 255, False, 2, False)
@@ -311,12 +307,12 @@ class lora_bladerf(gr.top_block, Qt.QWidget):
         self.lora_sdr_deinterleaver_0_0_0 = lora_sdr.deinterleaver( soft_decoding)
         self.lora_sdr_deinterleaver_0_0 = lora_sdr.deinterleaver( soft_decoding)
         self.lora_sdr_deinterleaver_0 = lora_sdr.deinterleaver( soft_decoding)
-        self.lora_sdr_crc_verif_0_1 = lora_sdr.crc_verif( False, False)
-        self.lora_sdr_crc_verif_0_0_1 = lora_sdr.crc_verif( False, False)
-        self.lora_sdr_crc_verif_0_0_0_0 = lora_sdr.crc_verif( True, False)
-        self.lora_sdr_crc_verif_0_0_0 = lora_sdr.crc_verif( False, False)
-        self.lora_sdr_crc_verif_0_0 = lora_sdr.crc_verif( True, False)
-        self.lora_sdr_crc_verif_0 = lora_sdr.crc_verif( True, False)
+        self.lora_sdr_crc_verif_0_1 = lora_sdr.crc_verif( 1, False)
+        self.lora_sdr_crc_verif_0_0_1 = lora_sdr.crc_verif( 1, False)
+        self.lora_sdr_crc_verif_0_0_0_0 = lora_sdr.crc_verif( 1, False)
+        self.lora_sdr_crc_verif_0_0_0 = lora_sdr.crc_verif( 1, False)
+        self.lora_sdr_crc_verif_0_0 = lora_sdr.crc_verif( 1, False)
+        self.lora_sdr_crc_verif_0 = lora_sdr.crc_verif( 1, False)
         self.blocks_multiply_xx_0_0_0 = blocks.multiply_vcc(1)
         self.blocks_multiply_xx_0_0_0.set_min_output_buffer(65568)
         self.blocks_multiply_xx_0_0 = blocks.multiply_vcc(1)
@@ -325,9 +321,9 @@ class lora_bladerf(gr.top_block, Qt.QWidget):
         self.blocks_multiply_xx_0.set_min_output_buffer(65568)
         self.blocks_conjugate_cc_0_0 = blocks.conjugate_cc()
         self.blocks_conjugate_cc_0 = blocks.conjugate_cc()
-        self.analog_sig_source_x_0_1 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, -500000, 1, 0, 0)
-        self.analog_sig_source_x_0_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, -300000, 1, 0, 0)
-        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, -100000, 1, 0, 0)
+        self.analog_sig_source_x_0_1 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, (-500000), 1, 0, 0)
+        self.analog_sig_source_x_0_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, (-300000), 1, 0, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, (-100000), 1, 0, 0)
 
 
         ##################################################
@@ -342,8 +338,8 @@ class lora_bladerf(gr.top_block, Qt.QWidget):
         self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 0))
         self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_multiply_xx_0_0, 0))
         self.connect((self.analog_sig_source_x_0_1, 0), (self.blocks_multiply_xx_0_0_0, 0))
-        self.connect((self.blocks_conjugate_cc_0, 0), (self.osmosdr_sink_0_0, 0))
-        self.connect((self.blocks_conjugate_cc_0_0, 0), (self.osmosdr_sink_0_0_0, 0))
+        self.connect((self.blocks_conjugate_cc_0, 0), (self.uhd_usrp_sink_0_0, 0))
+        self.connect((self.blocks_conjugate_cc_0_0, 0), (self.uhd_usrp_sink_0, 0))
         self.connect((self.blocks_multiply_xx_0, 0), (self.low_pass_filter_0, 0))
         self.connect((self.blocks_multiply_xx_0_0, 0), (self.low_pass_filter_0_0, 0))
         self.connect((self.blocks_multiply_xx_0_0_0, 0), (self.low_pass_filter_0_0_0, 0))
@@ -413,17 +409,17 @@ class lora_bladerf(gr.top_block, Qt.QWidget):
         self.connect((self.low_pass_filter_0_0, 0), (self.lora_sdr_frame_sync_0_0_1, 0))
         self.connect((self.low_pass_filter_0_0_0, 0), (self.lora_sdr_frame_sync_0_0_0, 0))
         self.connect((self.low_pass_filter_0_0_0, 0), (self.lora_sdr_frame_sync_0_0_0_0, 0))
-        self.connect((self.osmosdr_source_1, 0), (self.blocks_multiply_xx_0, 1))
-        self.connect((self.osmosdr_source_1, 0), (self.blocks_multiply_xx_0_0, 1))
-        self.connect((self.osmosdr_source_1, 0), (self.blocks_multiply_xx_0_0_0, 1))
-        self.connect((self.osmosdr_source_1, 0), (self.qtgui_freq_sink_x_0_1, 0))
-        self.connect((self.osmosdr_source_1, 0), (self.qtgui_waterfall_sink_x_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_multiply_xx_0_0, 1))
+        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_multiply_xx_0_0_0, 1))
+        self.connect((self.uhd_usrp_source_0, 0), (self.qtgui_freq_sink_x_0_1, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.qtgui_waterfall_sink_x_0, 0))
         self.connect((self.zeromq_pull_source_0, 0), (self.lora_sdr_whitening_0, 0))
         self.connect((self.zeromq_pull_source_0_0, 0), (self.lora_sdr_whitening_0_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "lora_bladerf")
+        self.settings = Qt.QSettings("GNU Radio", "lora_b210")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
@@ -451,8 +447,6 @@ class lora_bladerf(gr.top_block, Qt.QWidget):
 
     def set_sink_freq2(self, sink_freq2):
         self.sink_freq2 = sink_freq2
-        self.osmosdr_sink_0_0.set_center_freq(self.sink_freq2, 0)
-        self.osmosdr_sink_0_0_0.set_center_freq(self.sink_freq2, 0)
 
     def get_sf(self):
         return self.sf
@@ -472,6 +466,9 @@ class lora_bladerf(gr.top_block, Qt.QWidget):
         self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
         self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
         self.analog_sig_source_x_0_1.set_sampling_freq(self.samp_rate)
+        self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
+        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
+        self.uhd_usrp_sink_0_0.set_samp_rate(self.samp_rate)
 
     def get_pay_len(self):
         return self.pay_len
@@ -510,23 +507,28 @@ class lora_bladerf(gr.top_block, Qt.QWidget):
         self.center_freq = center_freq
         self.qtgui_freq_sink_x_0_1.set_frequency_range(self.center_freq, 2000000)
         self.qtgui_waterfall_sink_x_0.set_frequency_range(self.center_freq, 2000000)
+        self.uhd_usrp_sink_0.set_center_freq(self.center_freq, 0)
+        self.uhd_usrp_source_0.set_center_freq(self.center_freq, 0)
+        self.uhd_usrp_sink_0_0.set_center_freq(self.center_freq, 0)
 
     def get_bw(self):
         return self.bw
 
     def set_bw(self, bw):
         self.bw = bw
-        self.osmosdr_sink_0_0.set_bandwidth(self.bw, 0)
-        self.osmosdr_sink_0_0_0.set_bandwidth(self.bw, 0)
+
+    def get_bandwidth(self):
+        return self.bandwidth
+
+    def set_bandwidth(self, bandwidth):
+        self.bandwidth = bandwidth
+        self.uhd_usrp_source_0.set_bandwidth(self.bandwidth, 0)
 
 
 
 
-def main(top_block_cls=lora_bladerf, options=None):
+def main(top_block_cls=lora_b210, options=None):
 
-    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
     qapp = Qt.QApplication(sys.argv)
 
     tb = top_block_cls()
